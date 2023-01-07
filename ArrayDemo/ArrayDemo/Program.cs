305ae3b5e776a142e5cb4eb65f6c6aca5bd3a819
vakhtangi44 <102156467+vakhtangi44@users.noreply.github.com>
2023-01-07 16:04:56 +0400

    Update Program.cs

--- ArrayDemo/ArrayDemo/Program.cs
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -7,15 +7,16 @@
            var numbers = new [] { 3, 7, 9, 2, 14, 6 };
             // Length
             Console.WriteLine("length: " + numbers.Length);
-
+            
             // index of ()
             var index = Array.IndexOf(numbers, 9);
             Console.WriteLine("Index of 9 is: " + index);
-
+            
             // Clear ()
             Array.Clear(numbers, 0, 2);
             foreach (var n in numbers)
                 Console.WriteLine(n);
+            
             // Copy
             int[] another = new int[3];
             Array.Copy(numbers, another, 3);
@@ -28,13 +29,12 @@
             Console.WriteLine("Effect of Sort ()");
             foreach (var n in numbers)
                 Console.WriteLine(n);
+            
             // Reverse ()
             Array.Reverse(numbers);
-            Console.WriteLine("Effect of Copy ()");
+            Console.WriteLine("Effect of Reverse ()");
             foreach (var n in numbers)
                 Console.WriteLine(n);
-
-
         }
     }
 }
